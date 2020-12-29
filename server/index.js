ea39394b3da266df7f77d1599c9402d69317c855
Julian Bull <julianbullmagic@gmail.com>
2020-12-29 18:34:12 +1100

    hhhhh

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const config = require("./config/key");
 const path=require("path")
 
 const mongoose = require("mongoose");
-const connect = mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+const connect = mongoose.connect(config.mongoURI||process.env.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   // .then(() => console.log('MongoDB Connected...'))
   // .catch(err => console.log(err));
 
