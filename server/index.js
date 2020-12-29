aa917b5dc8675eda8e1653774986f24b24beeef1
Julian Bull <julianbullmagic@gmail.com>
2020-12-29 16:46:16 +1100

    kjshkjhdaskj

--- server/index.js
+++ server/index.js
@@ -7,8 +7,8 @@ const path=require("path")
 
 const mongoose = require("mongoose");
 const connect = mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB Connected...'))
-  .catch(err => console.log(err));
+  // .then(() => console.log('MongoDB Connected...'))
+  // .catch(err => console.log(err));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -40,5 +40,5 @@ if (process.env.NODE_ENV === "production") {
 const port = process.env.PORT || 5000
 
 app.listen(port, () => {
-  console.log(`Server Running at ${port}`)
+  // console.log(`Server Running at ${port}`)
 });
