6806c45facf46f1a563b760385de8a6cbe79d8c4
Julian Bull <julianbullmagic@gmail.com>
2020-12-29 17:08:57 +1100

    jsdjhfdkj

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const path=require("path")
-const { User } = require(path.join(__dirname, "/models/user"));
+const { User } = require(path.join("/app/server/models/user"));
 
 const { auth } = require("../middleware/auth");
 
