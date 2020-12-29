f2a356a0b12522eebc1b88a3cd3f281e2d202f52
Julian Bull <julianbullmagic@gmail.com>
2020-12-29 18:04:04 +1100

    nbbbv

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { User } = require("../models/user");
+const { User } = require("./models/user");
 
-const { auth } = require("../middleware/auth");
+const { auth } = require("./middleware/auth");
 const path=require("path")
 
 //=================================
